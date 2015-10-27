543a1e31490563a1c3ecf6f0a68cd7585171e938
Víctor R. Escobar <victor.r.e.o@gmail.com>
2015-10-27 10:31:44 +0100

    Added support for array checkings of enums

--- tests/TestHelper.hx
+++ tests/TestHelper.hx
@@ -1,11 +1,12 @@
+import Array;
 import hxlisp.Atom;
 
 
 class TestHelper extends haxe.unit.TestCase {
     public function assertArrays(arrA:Array<Dynamic>, arrB:Array<Dynamic>) {
-        return this.assertTrue(_assertArrays(arrA, arrB));
+        return this.assertTrue(_assertArrays(arrA, arrB, function(a,b) {return a!=b;}));
     }
-    private function _assertArrays(arrA:Array<Dynamic>,arrB:Array<Dynamic>):Bool {
+    private function _assertArrays(arrA:Array<Dynamic>,arrB:Array<Dynamic>, asserter):Bool {
         if (arrA.length != arrB.length) {
             trace('Both arrays have different size (${arrA.length} and ${arrB.length}):\n${arrA}\n${arrB}');
             return false;
@@ -16,13 +17,13 @@ class TestHelper extends haxe.unit.TestCase {
                     trace('Subtype is different at ${pos}:\n' + arrA + "\n" + arrB);
                     return false;
                 }
-                if (!_assertArrays(arrA[pos], arrB[pos])) {
+                if (!_assertArrays(arrA[pos], arrB[pos], asserter)) {
                     trace("Nested Arrays differ:\n" + arrA[pos] + "\n" + arrB[pos]);
                     return false;
                 }
                 continue;
             }
-            if (arrA[pos]!= arrB[pos]) {
+            if (asserter(arrA[pos], arrB[pos])) {
                 trace('Arrays differ in position ${pos}:\n'+ arrA + "\n" + arrB);
                 return false;
             }
@@ -30,12 +31,19 @@ class TestHelper extends haxe.unit.TestCase {
         return true;
     }
 
-    public function assertTypes(original_atom:Atom, atom:Dynamic){
-        switch original_atom {
-            case Atom.Number(n): assertEquals(n,atom);
-            case Atom.Symbol(s):  assertEquals(s, atom);
+    public function assertArrayTypes(original_atom:Array<Atom>, atomArr:Array<Dynamic>){
+        return this.assertTrue(_assertArrays(original_atom, atomArr, function(a,b){return enumExtract(a)!=b;}));
+    }
+    public function assertTypes(original_atom:Atom, bare_type:Dynamic){
+        return assertEquals(enumExtract(original_atom), bare_type);
+    }
+    private function enumExtract(original_atom:Atom):Dynamic{
+        return switch original_atom {
+            case Atom.Number(n): n;
+            case Atom.Symbol(s): s;
             //case Atom.List(l):  assertArrays(l, atom);
         }
+
 
     }
 }
